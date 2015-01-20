b97abfc35258a0a909fa48ff271d04b7ade56cfb
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-01-20 12:19:03 +1100

    timeout added for data load for production

--- client/app/data/data.controller.js
+++ client/app/data/data.controller.js
@@ -62,13 +62,21 @@ angular.module('serveMeApp')
 
 
     //call table service 
-    dataSrv.tableDisplay("/assets/dataDir/data.json","JSON",".col-md-12",$scope.prepare_Redditdata,$scope.addredditColumn);
+    setTimeout(function(){
+       dataSrv.tableDisplay("/assets/dataDir/data.json","JSON",".col-md-12",$scope.prepare_Redditdata,$scope.addredditColumn);
+    },1200);
+   
     // dataSrv.tableDisplay("/api/things/","JSON",".col-md-12",$scope.prepare_thingsdata,$scope.addThingsColumn);  
 
     //call scatterplot service
-    dataSrv.scatterPlotDisplay("/assets/dataDir/data.json","JSON","#svg3",$scope.prepare_scatterdata); 
-
+    setTimeout(function(){
+ 	  dataSrv.scatterPlotDisplay("/assets/dataDir/data.json","JSON","#svg3",$scope.prepare_scatterdata); 
+    },1200);
+   
     //call Brush service
- 	dataSrv.brushDisplay("/assets/dataDir/data.json","JSON","#svg4",$scope.prepare_scatterdata); 
+    setTimeout(function(){
+       dataSrv.brushDisplay("/assets/dataDir/data.json","JSON","#svg4",$scope.prepare_scatterdata); 
+    },1200);
+ 	
   	
   }]);
