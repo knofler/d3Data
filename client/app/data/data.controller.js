b26c8783acf97b1cda100a6ad3a04dbfee6a4472
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-01-20 12:52:20 +1100

    delay timeout

--- client/app/data/data.controller.js
+++ client/app/data/data.controller.js
@@ -62,14 +62,21 @@ angular.module('serveMeApp')
 
 
     //call table service 
+    setTimeout(function(){
        dataSrv.tableDisplay("assets/dataDir/data.json","JSON",".col-md-12",$scope.prepare_Redditdata,$scope.addredditColumn);
+    },4200);
+   
     // dataSrv.tableDisplay("/api/things/","JSON",".col-md-12",$scope.prepare_thingsdata,$scope.addThingsColumn);  
 
     //call scatterplot service
+    setTimeout(function(){
  	  dataSrv.scatterPlotDisplay("assets/dataDir/data.json","JSON","#svg3",$scope.prepare_scatterdata); 
-
+    },4200);
+   
     //call Brush service
+    setTimeout(function(){
        dataSrv.brushDisplay("assets/dataDir/data.json","JSON","#svg4",$scope.prepare_scatterdata); 
+    },4200);
  	
   	
   }]);
