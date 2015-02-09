c84fc6209433e8ea8130405f403f999de1363d09
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-02-09 17:39:36 +1100

    chart is visible

--- client/app/c3/c3.controller.js
+++ client/app/c3/c3.controller.js
@@ -1,6 +1,58 @@
 'use strict';
 
 angular.module('serveMeApp')
-  .controller('C3Ctrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('C3Ctrl', function ($scope,$http,socket) {
+    
+	// Generate basic chart with static data by c3
+	$scope.bChart = c3.generate({
+	  bindto: '#bChart',
+	  data: {
+	  	columns: [
+	       ['data1', 30, 200, 100, 400, 150, 250],
+	       ['data2', 50, 20, 10, 40, 15, 25]
+	  	]
+	   }
+	 });
+
+	// Introduce additional axis for data2. Add data.axes and axis.y2.show as follows:
+	$scope.cChart = c3.generate({
+		bindto:'#cChart',
+		data:{
+			columns: [
+				['data1',30, 200, 100, 400, 150, 250],
+				['data2',50, 20, 10, 40, 15, 25]
+			],
+			axes:{
+				data2:'y2' //Add
+			},
+			types:{
+				data2:'bar' //Add
+			}
+		},
+		axis:{
+			y:{
+				label:{ //Add
+					text:'Y label',
+					position:'outer-middle'
+				},
+				tick:{
+					format:d3.format("$,") //ADD
+				}
+			},
+			y2:{
+				show:true,
+				label:{ //Add
+					text:'Y2 label',
+					position:'outer-middle'
+				}
+				
+			}
+		}
+	 });
+
+
+
+
+
+
   });
