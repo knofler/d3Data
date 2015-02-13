4291e813c3f218b67c08c86b85ec3e3b90082517
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-02-13 17:35:05 +1100

    api data load done

--- client/app/c3/c3.controller.js
+++ client/app/c3/c3.controller.js
@@ -50,9 +50,56 @@ angular.module('serveMeApp')
 		}
 	 });
 
+	// By using load() API, you can load data and update the chart dynamically as follows:
+	// *****************Generate chart without data********************
+	$scope.apiChart = c3.generate({
+		bindto:'#apiChart',
+		data:{
+			columns: [
+				['data1',30, 200, 100, 400, 150, 250],
+				['data2',50, 20, 10, 40, 15, 25]
+			],
+			axes:{
+				data2:'y2' //Add
+			},
+			types:{
+				data2:'bar' //Add
+			}
+		},
+		axis:{
+			y:{
+				label:{ //Add
+					text:'Y label',
+					position:'outer-middle'
+				},
+				tick:{
+					format:d3.format("$,") //ADD
+				}
+			},
+			y2:{
+				show:true,
+				label:{ //Add
+					text:'Y2 label',
+					position:'outer-middle'
+				}
+				
+			}
+		}
+	 });
 
-
-
+	// ************Load Data **************************
+	$scope.loadData = function(){
+	  $scope.apiChart.load({
+		columns:[
+		  ['data1',300, 100, 250, 150, 300, 150, 500],
+		  ['data2', 100, 200, 150, 50, 100, 250]
+		 ],
+		 types:{
+				data2:'line' //Add
+			}
+	    })
+	 };
+	
 
 
   });
